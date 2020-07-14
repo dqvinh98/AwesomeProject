e29a16ecf76f21033378913bc60436d3f5d440f2
dqvinh98 <dqvinh98@gmail.com>
2020-07-14 09:06:47 +0700

    update app.js

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,21 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, View, Text, FlatList, Alert} from 'react-native';
+import Header from './TodoApp/todoAppComponents/Header';
+import CounterContainer from './TodoApp/todoAppContainers/CounterContainer';
+import TaskListContainer from './TodoApp/todoAppContainers/TaskListContainer';
+import AddTodoContainer from './TodoApp/todoAppContainers/AddTodoContainer';
 
-export default function App() {
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import {connect, Provider} from 'react-redux';
+import taskListReducer from './TodoApp/reducers/taskListReducer';
+import numberReducer from './TodoApp/reducers/numberReducer';
+
+// Store
+const store = createStore (combineReducers({taskList: taskListReducer, number: numberReducer}));
+
+export default function App () {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.content}>
+          <AddTodoContainer  />
+          <CounterContainer />
+          <View style={styles.list}>
+            <TaskListContainer />
+          </View>
+        </View>
+      </View>
+    </Provider>
   );
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create ({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  content: {
+    padding: 40,
+    flex: 1,
+  },
+  list: {
+    marginTop: 20,
+    flex: 1,
   },
 });
